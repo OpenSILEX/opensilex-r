72d0008fef0a11cd42c660f3dc8aae6e83cbf9af
chabrault <37081971+chabrault@users.noreply.github.com>
2022-07-26 17:53:49 +0200

    Change output colnames and rownames
    1. Fix problem with duplicated row names & change sep
    2. Add parameter to control header with 3 possibilities: initial, translated and both (concatenated names)

--- R/ModalityOS.R
+++ R/ModalityOS.R
@@ -16,7 +16,9 @@ get_scientific_object_modalities <- function(host,
                                              user,
                                              password,
                                              experiment_uri,
-                                             scientific_object_type) {
+                                             scientific_object_type,
+                                             header_type="initial") {
+  stopifnot(header_type %in% c("initial","translated","both"))
   token <- opensilexR::get_token(
     host = host,
     user = user,
@@ -48,16 +50,23 @@ get_scientific_object_modalities <- function(host,
   header_lines <- utils::read.csv(
     text = post_result_text,
     nrows = 2,
-    header = FALSE
+    header = FALSE,
+    sep = ";"
   )
-  header <- paste(header_lines[1, ],
-    header_lines[2, ],
-    sep = "/"
-  )
+  if(header_type == "initial"){
+    header <- header_lines[1,]
+  } else if(header_type == "translated"){
+    header <- header_lines[2,]
+  } else if(header_type == "both"){
+    header <- paste(header_lines[1, ], header_lines[2, ], sep = "/")
+  }
   result_df <- read.csv(
     text = post_result_text,
     skip = 2,
-    col.names = header
+    col.names = header,
+    row.names = NULL,
+    sep = ";",
+    check.names = FALSE
   )
   return(result_df)
 }
